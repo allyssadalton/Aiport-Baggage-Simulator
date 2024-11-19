996476e6a2e13516967ad6e252461d44cbd27205
Allyssa Dalton <allyssamarie06@icloud.com>
2024-11-19 22:45:59 +0000

    Check Comments

--- Flight.h
+++ Flight.h
@@ -2,6 +2,7 @@
 #define FLIGHT_H
 
 #include <string>
+#include <iostream>
 using namespace std; 
 
 class Flight{
@@ -9,31 +10,19 @@ private:
     string flightID;
     string departurePlace;
     string arrivalPlace;
-    chrono::system_clock::time_point departureTime; // Credits to ChatGPT
-    chrono::system_clock::time_point arrivalTime;  //  Credits to ChatGPT
+    //departure time
+    //arrival time
     string planeType;
-    int capacity; 
+    int capacity;
     string status;
-    string overheadBinSpace[118]; //Array in which each element is a bagID
-    //NEEDS STACK FOR CHECKED BAGS ON THE FLIGHT 
 
 public:
-    Flight(const string &flight, const string &departure, const string &destination, const chrono::system_clock::time_point &dtime, const chrono::system_clock::time_point &atime,
-    const string &plane, int cap);
-    string getFlightID();
-    string getdeparturePlace();
-    string getarrivalPlace();
-    chrono::system_clock::time_point getdepartureTime();
-    chrono::system_clock::time_point getarrivalTime();
-    string getPlaneType();
-    int getCapacity();
-    string getStatus();
-    string getOverheadBin();
-    int overheadBinSpace();
-    void assignBagToFlight(Bag& bag); //NOT IN CPP
-    void setStatus(string newstatus);
-    void updateOverheadBin(string bagIDAdded);
-    void displayFlightInfo();
+    string getFlightStatus();
+    void updateFlightStatus(newStatus);
+    //baglist, idk datatype
+    //getPassengerManifest, idk datatype
+
+
 
 }
 
